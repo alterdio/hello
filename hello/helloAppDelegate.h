48f8cc5be061ff45af6c3e6a181fe7e2b7045ee9
Dhi Aurrahman <dio.rahman@gmail.com>
2011-06-24 23:12:54 +0700

    commit to branch

--- hello/helloAppDelegate.h
+++ hello/helloAppDelegate.h
@@ -1,6 +1,6 @@
 //
 //  helloAppDelegate.h
-//  hello, I'm making change, yet another changes
+//  hello, I'm making change, yet another changes to branch
 //
 //  Created by Alter Dio on 6/24/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
